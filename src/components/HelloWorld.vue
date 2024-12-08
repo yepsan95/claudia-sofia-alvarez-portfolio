b651eb964e2c4d238b48dd3c67519ab043f7ce5c
Bryan Yep <yepsan95@gmail.com>
2024-12-08 10:36:48 -0500

    fix: tailwindcss now works throughout the whole project [3]

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ const count = ref(0);
 <template>
   <h1>{{ msg }}</h1>
 
-  <div class="card">
+  <div class="card bg-red-500 border-red-900">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
