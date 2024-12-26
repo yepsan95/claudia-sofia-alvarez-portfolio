5ce31a8335b307174f3cf4b75c7d18fbf0e4b0e7
Bryan Yep <yepsan95@gmail.com>
2024-12-26 12:47:50 -0500

    feat: create base-button component [5]

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button :class="buttonColor">
+    {{ props.message }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { computed, type PropType } from "vue";
+
+type ButtonType = "primary" | "secondary" | "cancel";
+
+const props = defineProps({
+  type: {
+    type: String as PropType<ButtonType>,
+    required: false,
+    default: () => "primary",
+  },
+  message: {
+    type: String,
+    required: false,
+    default: () => "",
+  },
+});
+
+const buttonColor = computed<string>(() => {
+  const colors = {
+    primary: "bg-sky-600",
+    secondary: "bg-sky-900",
+    cancel: "bg-gray-600",
+  };
+
+  return colors[props.type];
+});
+</script>
