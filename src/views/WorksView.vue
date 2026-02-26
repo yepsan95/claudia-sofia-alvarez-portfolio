546908907442c146c8775d2aa7347d2b256b0adc
Bryan Yep <yepsan95@gmail.com>
2026-02-26 18:00:28 -0500

    feat: add placeholder text with composer's name [31]

--- src/views/WorksView.vue
+++ src/views/WorksView.vue
@@ -1,6 +1,6 @@
 <template>
   <base-timeline
-    title="Beethoven's Works By Year"
+    title="Claudia Sofia Alvarez' Works By Year"
     :subtitle="subtitle"
     :timeline-data="worksByYear"
     :sidebar-header="sidebarHeader"
@@ -12,10 +12,12 @@ import BaseTimeline from "../components/BaseTimeline.vue";
 import worksByYear from "../data/worksByYear.json" with { type: "json" };
 
 const subtitle = `
-  Ludwig van Beethoven was a German composer and pianist from the Classical to Romantic eras,
-  widely considered one of history's greatest composers. He revolutionized music with works like his nine symphonies and thirty-two piano sonatas,
-  transitioning Western art music from the Classical to the Romantic period. Despite experiencing deafness in his adult life, he continued to compose,
-  creating some of his most celebrated pieces during this challenging time.
+  Claudia Sofia Alvarez is a Peruvian composer Lorem ipsum dolor sit amet,
+  consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+  laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+  in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+  cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 `;
 
 const sidebarHeader = "Works By Year";
